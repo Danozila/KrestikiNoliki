7aafab40345aa5543b86a7a3f424abde0b1eddfa
Danya <botalov.daniil@yandex.ru>
2025-01-18 00:47:34 +0500

    Создание функции для вывода поля

--- KrestikiNoliki.py
+++ KrestikiNoliki.py
@@ -1,1 +1,11 @@
-
+def showField(field):
+    print()
+    for i in range(3):
+        for j in range(3):
+            print(" |" + field[i][j] + "| ", end='')
+        print()
+        print()
+field = [["x", "o", " "],
+         ["x", " ", "o"],
+         ["o", "x", " "]]
+showField(field)
